4f06ad7dfccb37fcceccb635fc37b2c445793d29
cdxing <danielxchenx@gmail.com>
2020-04-13 01:38:44 -0400

    QA plots

--- PicoAnalyzer.C
+++ PicoAnalyzer.C
@@ -70,18 +70,23 @@
 
 //////////////////////////////// Main Function /////////////////////////////////
 void PicoAnalyzer(const Char_t *inFile = "/star/data01/pwg/dchen/Ana/fxtPicoAna/files/PicoDst/st_physics_16140033_raw_0000002.picoDst.root",
-                      TString outFile = "test_EpdEP"//,
-                      //Int_t   inputp1 = 1
+                      TString outFile = "test_EpdEP",
+                      Int_t   inputp1 = 1
                     )
 {
 
-  //Int_t EpOrder = inputp1; // EpOrder = 1, 2, 3
-
+  Int_t EpOrder = inputp1; // Event plane Fourier expansion order = 1, 2, 3
+  int mEvtcut[5] = {0};
+  int mTrkcut[6] = {0};
+  // (0) ================== Read input files and set status =====================
   StPicoDstReader* picoReader = new StPicoDstReader(inFile);
   picoReader->Init();
   std::cout << "Explicit read status for some branches" << std::endl;
   picoReader->SetStatus("*",0);
   picoReader->SetStatus("Event",1);
+  picoReader->SetStatus("Track",1);
+  picoReader->SetStatus("BTofHit",1);
+  picoReader->SetStatus("BTofPidTraits",1);
   picoReader->SetStatus("EpdHit",1);
 
   if( !picoReader->chain() ) {
@@ -90,11 +95,11 @@ void PicoAnalyzer(const Char_t *inFile = "/star/data01/pwg/dchen/Ana/fxtPicoAna/
 
   Long64_t eventsInTree = picoReader->tree()->GetEntries();
   std::cout << "eventsInTree: "  << eventsInTree << std::endl;
+  // ------------------ Get event numbers from TChain --------------------------------
   Long64_t events2read = picoReader->chain()->GetEntries();
   std::cout << "Number of events to read: " << events2read << std::endl;
 
-  outFile.Append(".picoDst.result.root");
-  // EPD EP finder to get EPD event plane
+  // (1) ================= Set up EPD EP finder to get EPD event plane ============
   TString EpdEpOutputName = "EpdEpCorrectionHistograms_OUTPUT_";
   EpdEpOutputName += outFile;
   EpdEpOutputName += ".root";
@@ -105,80 +110,271 @@ void PicoAnalyzer(const Char_t *inFile = "/star/data01/pwg/dchen/Ana/fxtPicoAna/
   mEpFinder->SetMaxTileWeight(3.0);     // recommended by EPD group 3.0
   TClonesArray * mEpdHits = new TClonesArray("StPicoEpdHit");
   unsigned int found;
-  // Retrieve picoDst TChain*
+  // --------------------- Retrieve EpdHits TClonesArray ----------------------------
   TChain *mPicoDst = picoReader->chain();
   mPicoDst->SetBranchStatus("EpdHit*",1,&found);   // note you need the asterisk
-  std::cout << "EpdHit Branch returned found= " << found << std::endl;
+  std::cout << "EpdHit Branch returned found= " << found << std::endl; // ? What is the EpdHit branch ? Check it on StRoot.
   mPicoDst->SetBranchAddress("EpdHit",&mEpdHits);
 
-  // output root files
+  // (2) ================ Output files and histograms ==========================
+  outFile.Append(".picoDst.result.root");
   TFile *outputFile = new TFile(outFile,"recreate");
-  TH2D *hEastRingRawQDotProduct = new TH2D("hEastRingRawQDotProduct","EPD east Raw Q dot product between different rings",120,0.,120.,400,-2.0,2.0);
-  hEastRingRawQDotProduct->GetXaxis()->SetTitle("(Ring_{a},Ring_{b})");
-  hEastRingRawQDotProduct->GetYaxis()->SetTitle("Dot Product");
-  TH2D *hEastRingWeightedQDotProduct = new TH2D("hEastRingWeightedQDotProduct","EPD east Weighted Q dot product between different rings",120,0.,120.,400,-2.0,2.0);
-  hEastRingWeightedQDotProduct->GetXaxis()->SetTitle("(Ring_{a},Ring_{b})");
-  hEastRingWeightedQDotProduct->GetYaxis()->SetTitle("Dot Product");
-  int iBin = 0;
-  for(int i = 0;i<15;i++){
-    for(int j = i+1; j<16;j++){
-      iBin++;
-      hEastRingRawQDotProduct->GetXaxis()->SetBinLabel(iBin,Form("(%d, %d)",i+1,j+1));
-      hEastRingWeightedQDotProduct->GetXaxis()->SetBinLabel(iBin,Form("(%d, %d)",i+1,j+1));
-    }
-  }
-  TProfile *tpEastRingRawQDotProduct = nullptr;
-  TProfile *tpEastRingWeightedQDotProduct = nullptr;
-
-  // Event loop
+  // ------------------- Event cuts QA histograms ------------------------------
+  TH1D  *hist_runId = new TH1D("hist_runId","Event runId",4001,-0.5,4000.5);
+  TH1D *hist_Vz_pri = new TH1D("hist_Vz_pri","V_{Z} [cm]",6000,-300.0,300.0);
+  TH2D *hist_VyVx_pri = new TH2D("hist_VyVx_pri","V_{Y} [cm] vs. V_{X} [cm]",500,-5.0,5.0,500,-5.0,5.0);
+  TH1D *hist_Vr_pri = new TH1D("hist_Vr_pri","V_{R} [cm]",500,0.0,20.0);
+  TH1D *hist_triggerID = new TH1D("hist_triggerID","Event TriggerId",801,-0.5,800.5);
+  TH1D *hist_Vz_cut = new TH1D("hist_Vz_cut","V_{Z} after cut [cm]",6000,-300.0,300.0);
+  TH1D *hist_Vr_cut = new TH1D("hist_Vr_cut","V_{R} after cut [cm]",500,0.0,20.0);
+  TH2D *hist_VyVx_cut = new TH2D("hist_VyVx_cut","V_{Y} [cm] vs. V_{X} after cut [cm]",500,-5.0,5.0,500,-5.0,5.0);
+  // -------------------- Track loop QA histograms --------------------------------
+  TH2D *hist_px_py=new TH2D("hist_px_py","hist_px_py",4000,-10.0,10.0,4000,-10.0,10.0);
+  TH1D *hist_pz = new TH1D("hist_pz","p_{z} [GeV/c]",2000,0.0,10.0);
+  TH1D *hist_pt = new TH1D("hist_pt","p_{T} [GeV/c]",2000,0.0,10.0);
+  TH1D *hist_mom = new TH1D("hist_mom","p_{mom} [GeV/c]",2000,0.0,10.0);
+  TH1D *hist_mass2 = new TH1D("hist_mass2","hist_mass2",4000,-10.0,10.0);
+  TH1D *hist_eta = new TH1D("hist_eta","#eta",200,-3.0,0.5);
+  TH1D *hist_ratio = new TH1D("hist_ratio","hist_ratio",100,0,2);
+  TH1D *hist_nHits = new TH1D("hist_nHits","hist_nHits",100,-0.5,99.5);
+  TH1D *hist_ndEdx = new TH1D("hist_ndEdx","hist_ndEdx",100,-0.5,99.5);
+  TH1D *hist_DCA = new TH1D("hist_DCA","hist_DCA",100,0,10.0);
+  TH2D *hist_px_py_cut=new TH2D("hist_px_py_cut","hist_px_py_cut",4000,-10.0,10.0,4000,-10.0,10.0);
+  TH1D *hist_pz_cut = new TH1D("hist_pz_cut","p_{z} [GeV/c]",2000,0.0,10.0);
+  TH1D *hist_pT_cut = new TH1D("hist_pT_cut","p_{T} [GeV/c]",2000,0.0,10.0);
+  TH1D *hist_mom_cut = new TH1D("hist_mom_cut","p_{mom} [GeV/c]",2000,0.0,10.0);
+  TH1D *hist_mass2_cut = new TH1D("hist_mass2_cut","hist_mass2_cut",4000,-10.0,10.0);
+  TH1D *hist_eta_cut = new TH1D("hist_eta_cut","#eta",200,-3.0,0.5);
+  TH1D *hist_ratio_cut = new TH1D("hist_ratio_cut","hist_ratio_cut",100,0,2);
+  TH1D *hist_nHits_cut = new TH1D("hist_nHits_cut","hist_nHits_cut",100,-0.5,99.5);
+  TH1D *hist_ndEdx_cut = new TH1D("hist_ndEdx_cut","hist_ndEdx_cut",100,-0.5,99.5);
+  TH1D *hist_DCA_cut = new TH1D("hist_DCA_cut","hist_DCA_cut",100,0,10.0);
+  // ------------------ Centrality QA histograms ----------------------------------
+  TH1D *hist_cent = new TH1D("hist_cent","Centrality",Ncentralities+1,-0.5,Ncentralities+0.5);
+  TH1D *hist_realTrackMult = new TH1D("hist_realTrackMult","Actual track multiplicity",1001,-0.5,1000.5);
+  TH2D *hist_realTrackMult_refmult = new TH2D("hist_realTrackMult_refmult","Actual track multiplicity vs. RefMult",1001,-0.5,1000.5,1001,-0.5,1000.5);
+  TH2D *hist_realTrackMult_grefmult = new TH2D("hist_realTrackMult_grefmult","Actual track multiplicity vs. gRefMult",1001,-0.5,1000.5,1001,-0.5,1000.5);
+  // (3) =========================== Event loop ====================================
   for(Long64_t iEvent=0; iEvent<events2read; iEvent++)
   {
+    // ---------------------- Event reading quality assurance ----------------------
     if((iEvent+1)%100 == 0) std::cout << "Working on event #[" << (iEvent+1)
     << "/" << events2read << "]" << std::endl;
-
     Bool_t readEvent = picoReader->readPicoEvent(iEvent);
     if( !readEvent ) {
         std::cout << "Something went wrong, Master! Nothing to analyze..."
         << std::endl;
         break;
     }
-
-    StPicoDst *dst = picoReader->picoDst();
-    // Retrieve event information
+    StPicoDst     *dst = picoReader->picoDst();
     StPicoEvent *event = dst->event();
     if( !event ) {
-        std::cout << "Something went wrong, Master! Event is hiding from me..." << std::endl;
+        std::cout << "Something went wrong, Master! Event is hiding from me..."
+        << std::endl;
         break;
     }
+    mEvtcut[0]++;// No event cut yet
 
-    TVector3 pVtx     = event->primaryVertex();
-    StEpdEpInfo result = mEpFinder->Results(mEpdHits,pVtx,0);  // and now you have all the EP info you could ever want :-)
+    // (4) =================== Get event parameters ================================
+    Int_t runId       = event->runId();
+    Int_t nTracks     = dst->numberOfTracks();
+    const Float_t   B = event->bField(); // Magnetic field
+    double d_MagField = event->bField();
+    Double_t Day      = (Double_t)runId - 19151028.0; // a day bin
+    hist_runId->Fill(Day);
 
-    TVector2 eastRingRawQ[16];
-    TVector2 eastRingWeightedQ[16];
-    for(int i=0; i<16; i++)
-    {
-      eastRingRawQ[i] = (TVector2) result.EastRingRawQ(1,i+1);
-      eastRingWeightedQ[i] = (TVector2) result.EastRingPhiWeightedQ(1,i+1);
-    }
+    Double_t primaryVertex_X    = (Double_t)event->primaryVertex().X();
+    Double_t primaryVertex_Y    = (Double_t)event->primaryVertex().Y();
+    Double_t primaryVertex_Z    = (Double_t)event->primaryVertex().Z();
+    Double_t primaryVertex_perp = (Double_t)event->primaryVertex().Perp();
+    hist_Vz_pri  ->Fill(primaryVertex_Z);
+    hist_VyVx_pri->Fill(primaryVertex_X,primaryVertex_Y);
+    hist_Vr_pri  ->Fill(primaryVertex_perp);
 
-    int ibin = 0;
-    for(int i = 0;i<15;i++){
-      TVector2 RawQa = eastRingRawQ[i];
-      TVector2 WeightedQa = eastRingWeightedQ[i];
-      for(int j = i+1; j<16;j++){
-        TVector2 RawQb = eastRingRawQ[j];
-        TVector2 WeightedQb = eastRingWeightedQ[j];
-        hEastRingRawQDotProduct->Fill(ibin,RawQa.X()*RawQb.X()+RawQa.Y()*RawQb.Y());
-        hEastRingWeightedQDotProduct->Fill(ibin,WeightedQa.X()*WeightedQb.X()+WeightedQa.Y()*WeightedQb.Y());
-        ibin++;
-        if(ibin == 1) std::cout << "first bin dot product = " << (RawQa.X()*RawQb.X()+RawQa.Y()*RawQb.Y()) << std::endl;
+    // ---------------------- trigger selection ---------------------------------
+    std::vector <unsigned int> triggerIDs;
+    triggerIDs.clear();
+    triggerIDs      = event->triggerIds();
+    bool b_bad_trig = true;
+    for(unsigned int i=0; i < triggerIDs.size(); i++)
+      {
+        Double_t d_trigger = (Double_t)triggerIDs[i] - 620050.0;
+        hist_triggerID->Fill(d_trigger);
+        if(triggerIDs[i] == 630802) b_bad_trig = false; // hlt_fixedTargetGood 7.2GeV
       }
+
+    // --------------------------- Vertex cut -----------------------------------
+    double      d_zvtx  = -9999.0;
+    double      d_xvtx  = -9999.0;
+    double      d_yvtx  = -9999.0;
+    double  d_vtx_perp  = -9999.0;
+    TVector3       pVtx = event->primaryVertex();
+    StEpdEpInfo  result = mEpFinder->Results(mEpdHits,pVtx,0);  // and now you have all the EP info you could ever want :-)
+    d_zvtx     = pVtx.z();
+    d_xvtx     = pVtx.x();
+    d_yvtx     = pVtx.y();
+    d_vtx_perp = pVtx.Perp();
+    bool b_bad_zvtx   =  ((d_zvtx < 199.0) || (d_zvtx > 202.0)); //FXT_26p5_2018
+    bool b_bad_xvtx   =  ((d_xvtx < -1.0) || (d_xvtx > 1.0)); //FXT_26p5_2018
+    bool b_bad_yvtx   =  ((d_yvtx < -3.0) || (d_yvtx > -0.5)); //FXT_26p5_2018
+    bool b_bad_rvtx   =  d_vtx_perp > 3.0;
+    bool b_bad_evt  = b_bad_zvtx || b_bad_trig || b_bad_xvtx || b_bad_yvtx || b_bad_rvtx;
+    if(b_bad_evt) continue;
+    hist_Vz_cut->Fill(primaryVertex_Z);
+    hist_Vr_cut->Fill(primaryVertex_perp);
+    hist_VyVx_cut->Fill(primaryVertex_X,primaryVertex_Y);
+    mEvtcut[1]++; // 1. vertex event cut
+
+    Int_t  refMult = event->refMult(); // refMult
+    Int_t grefMult = event->grefMult();
+
+    // (5) =============== Track loop to determine good tracks =================
+    Int_t centrality = 0;
+    bool a_b_cent[10]={false};
+    int nGoodTracks = 0;
+    for(Int_t iTrk=0; iTrk<nTracks; iTrk++){
+      StPicoTrack *picoTrack = dst->track(iTrk);
+      mTrkcut[0]++; // 0. No track cut
+      if(!picoTrack) continue;
+      mTrkcut[1]++; // 1. pico track cut
+      StPicoBTofPidTraits *trait = NULL;
+      // ----------------------- Physics values of tracks --------------------------
+      double        d_tofBeta    = -999.;
+      if(picoTrack->isTofTrack()) trait = dst->btofPidTraits( picoTrack->bTofPidTraitsIndex() );
+      if(trait)        d_tofBeta = trait->btofBeta();
+      double d_px  = picoTrack->gMom().x();
+      double d_py  = picoTrack->gMom().y();
+      double d_pz  = picoTrack->gMom().z();
+      double d_pT  = picoTrack->gPt();
+      double d_mom = sqrt(d_pT*d_pT + d_pz*d_pz);
+      double mass2 = d_mom*d_mom*((1.0/(d_tofBeta*d_tofBeta))-1.0);
+      Double_t eta = picoTrack->pMom().Eta();
+      // --------------- QA plots before major track cuts ----------------------
+      hist_px_py->Fill(d_px,d_py);
+      hist_pz   ->Fill(d_pz);
+      hist_pT   ->Fill(d_pT);
+      hist_mom  ->Fill(d_mom);
+      hist_mass2->Fill(mass2);
+      hist_eta  ->Fill(eta);
+      hist_ratio->Fill(((double)picoTrack->nHitsFit() / (double)picoTrack->nHitsPoss()));
+      hist_nHits->Fill((double)picoTrack->nHitsFit());
+      hist_ndEdx->Fill(picoTrack->nHitsDedx());
+      hist_DCA  ->Fill(picoTrack->gDCA(primaryVertex_X,primaryVertex_Y,primaryVertex_Z));
+
+      if(!picoTrack->isPrimary()) continue;
+      mTrkcut[2]++; // 2. Primary track cut
+      bool    b_bad_dEdx     = (picoTrack->nHitsDedx() <= 0);
+      bool    b_bad_tracking = (((double)picoTrack->nHitsFit() / (double)picoTrack->nHitsPoss()) < 0.52);
+      bool b_not_enough_hits = ((double)picoTrack->nHitsFit()) < 15;
+      bool    b_bad_DCA      = (picoTrack->gDCA(primaryVertex_X,primaryVertex_Y,primaryVertex_Z) >= 3.0);
+      bool    b_bad_track    = b_bad_dEdx || b_bad_tracking || b_not_enough_hits || b_bad_DCA;
+      if(b_bad_track) continue;
+      mTrkcut[3]++; // 3. Bad track cuts
+      nGoodTracks++; // nGoodTracks is used to determine centrality later in the event loop
+      // --------------- QA plots after major track cuts ----------------------
+      hist_px_py_cut->Fill(d_px,d_py);
+      hist_pz_cut   ->Fill(d_pz);
+      hist_pT_cut   ->Fill(d_pT);
+      hist_mom_cut  ->Fill(d_mom);
+      hist_mass2_cut->Fill(mass2);
+      hist_eta_cut  ->Fill(eta);
+      hist_ratio_cut->Fill(((double)picoTrack->nHitsFit() / (double)picoTrack->nHitsPoss()));
+      hist_nHits_cut->Fill((double)picoTrack->nHitsFit());
+      hist_ndEdx_cut->Fill(picoTrack->nHitsDedx());
+      hist_DCA_cut  ->Fill(picoTrack->gDCA(primaryVertex_X,primaryVertex_Y,primaryVertex_Z));
+      if(d_tofBeta0 == -999) continue;
+      mTrkcut[4]++; // 4. Bad tof track cut, to see how many tracks with tof information
+    } // Track loop to determine good tracks
+
+    // (6) ================ Centrality definition ===============================
+    Int_t centrality = 0;
+    bool a_b_cent[10]={false};
+    bool b_pileup   = (nGoodTracks > 270);
+    bool b_low_mult = (nGoodTracks < 10);
+    a_b_cent[0]     = (nGoodTracks >= 200); // 0 - 10%
+    a_b_cent[1]     = (nGoodTracks >= 150 && nGoodTracks < 200); // 10 - 20%
+    a_b_cent[2]     = (nGoodTracks >= 124 && nGoodTracks < 150); // 20 - 30%
+    a_b_cent[3]     = (nGoodTracks >= 100 && nGoodTracks < 124); // 30 - 40%
+    a_b_cent[4]     = (nGoodTracks >= 72  && nGoodTracks < 100); // 40 - 50%
+    a_b_cent[5]     = (nGoodTracks >= 50  && nGoodTracks < 72); // 50 - 60%
+    a_b_cent[6]     = (nGoodTracks >= 40  && nGoodTracks < 50); // 60 - 70%
+    a_b_cent[7]     = (nGoodTracks >= 30  && nGoodTracks < 40); // 70 - 80%
+    a_b_cent[8]     = (nGoodTracks >= 20  && nGoodTracks < 30); // 80 - 90%
+    a_b_cent[9]     = (nGoodTracks >= 10  && nGoodTracks < 20); // >90%
+    for(int i=0;i<10;i++){
+      if(a_b_cent[i]) centrality = ++i;
     }
+    hist_cent->Fill(centrality);
+    hist_realTrackMult->Fill(nGoodTracks);
+    hist_realTrackMult_refmult->Fill(nGoodTracks,refMult);
+    hist_realTrackMult_grefmult->Fill(nGoodTracks,grefMult);
 
   }  // Event Loop
-  tpEastRingRawQDotProduct = hEastRingRawQDotProduct->ProfileX();
-  tpEastRingWeightedQDotProduct = hEastRingWeightedQDotProduct->ProfileX();
+  // --------------------- Set histograms axises titles --------------------------------
+  hist_runId->GetXaxis()->SetTitle("RunId");
+  hist_runId->GetYaxis()->SetTitle("# of events");
+  hist_Vz_pri->GetXaxis()->SetTitle("V_{Z} [cm]");
+  hist_Vz_pri->GetYaxis()->SetTitle("# of events");
+  hist_VyVx_pri->GetXaxis()->SetTitle("V_{X} [cm]");
+  hist_VyVx_pri->GetYaxis()->SetTitle("V_{Y} [cm]");
+  hist_Vr_pri->GetXaxis()->SetTitle("V_{R} [cm]");
+  hist_Vr_pri->GetYaxis()->SetTitle("# of events");
+  hist_triggerID->GetXaxis()->SetTitle("TriggerID");
+  hist_triggerID->GetYaxis()->SetTitle("# of events");
+  hist_Vz_cut->GetXaxis()->SetTitle("V_{Z} [cm]");
+  hist_Vz_cut->GetYaxis()->SetTitle("# of events");
+  hist_Vr_cut->GetXaxis()->SetTitle("V_{R} [cm]");
+  hist_Vr_cut->GetYaxis()->SetTitle("# of events");
+  hist_VyVx_cut->GetXaxis()->SetTitle("V_{X} [cm]");
+  hist_VyVx_cut->GetYaxis()->SetTitle("V_{Y} [cm]");
+  hist_px_py->GetXaxis()->SetTitle("p_{x} [GeV/c]");
+  hist_px_py->GetYaxis()->SetTitle("p_{y} [GeV/c]");
+  hist_pz->GetXaxis()->SetTitle("p_{z} [GeV/c]");
+  hist_pz->GetYaxis()->SetTitle("# of tracks");
+  hist_pt->GetXaxis()->SetTitle("p_{T} [GeV/c]");
+  hist_pt->GetYaxis()->SetTitle("# of tracks");
+  hist_mom->GetXaxis()->SetTitle("p_{mom} [GeV/c]");
+  hist_mom->GetYaxis()->SetTitle("# of tracks");
+  hist_mass2->GetXaxis()->SetTitle("M^{2} [(GeV/c^{2})^{2}]");
+  hist_mass2->GetYaxis()->SetTitle("# of tracks");
+  hist_eta->GetXaxis()->SetTitle("#eta");
+  hist_eta->GetYaxis()->SetTitle("# of tracks");
+  hist_ratio->GetXaxis()->SetTitle("nHitsFit/nHitsPoss");
+  hist_ratio->GetYaxis()->SetTitle("# of Tracks");
+  hist_nHits->GetXaxis()->SetTitle("nHits");
+  hist_nHits->GetYaxis()->SetTitle("# of Tracks");
+  hist_ndEdx->GetXaxis()->SetTitle("nDedx");
+  hist_ndEdx->GetYaxis()->SetTitle("# of Tracks");
+  hist_DCA->GetXaxis()->SetTitle("DCA [cm]");
+  hist_DCA->GetYaxis()->SetTitle("# of Tracks");
+  hist_px_py_cut->GetXaxis()->SetTitle("p_{x} [GeV/c]");
+  hist_px_py_cut->GetYaxis()->SetTitle("p_{y} [GeV/c]");
+  hist_pz_cut->GetXaxis()->SetTitle("p_{z} [GeV/c]");
+  hist_pz_cut->GetYaxis()->SetTitle("# of tracks");
+  hist_pt_cut->GetXaxis()->SetTitle("p_{T} [GeV/c]");
+  hist_pt_cut->GetYaxis()->SetTitle("# of tracks");
+  hist_mom_cut->GetXaxis()->SetTitle("p_{mom} [GeV/c]");
+  hist_mom_cut->GetYaxis()->SetTitle("# of tracks");
+  hist_mass2_cut->GetXaxis()->SetTitle("M^{2} [(GeV/c^{2})^{2}]");
+  hist_mass2_cut->GetYaxis()->SetTitle("# of tracks");
+  hist_eta_cut->GetXaxis()->SetTitle("#eta");
+  hist_eta_cut->GetYaxis()->SetTitle("# of tracks");
+  hist_ratio_cut->GetXaxis()->SetTitle("nHitsFit/nHitsPoss");
+  hist_ratio_cut->GetYaxis()->SetTitle("# of Tracks");
+  hist_nHits_cut->GetXaxis()->SetTitle("nHits");
+  hist_nHits_cut->GetYaxis()->SetTitle("# of Tracks");
+  hist_ndEdx_cut->GetXaxis()->SetTitle("nDedx");
+  hist_ndEdx_cut->GetYaxis()->SetTitle("# of Tracks");
+  hist_DCA_cut->GetXaxis()->SetTitle("DCA [cm]");
+  hist_DCA_cut->GetYaxis()->SetTitle("# of Tracks");
+  hist_cent->GetXaxis()->SetTitle("Centrality bin");
+  hist_cent->GetYaxis()->SetTitle("# of events");
+  hist_realTrackMult->GetXaxis()->SetTitle("TrackMult");
+  hist_realTrackMult->GetXaxis()->SetTitle("# of events");
+  hist_realTrackMult_refmult->GetXaxis()->SetTitle("TrackMult");
+  hist_realTrackMult_refmult->GetXaxis()->SetTitle("RefMult");
+  hist_realTrackMult_grefmult->GetXaxis()->SetTitle("TrackMult");
+  hist_realTrackMult_grefmult->GetXaxis()->SetTitle("gRefMult");
   outputFile->Write();
   mEpFinder->Finish();
 }
