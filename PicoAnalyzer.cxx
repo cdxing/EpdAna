d10bb20747c5c769e84b3b09ca45e617ecf1292b
cdxing <danielxchenx@gmail.com>
2020-05-03 18:16:12 -0400

    usigned int for vector size

--- PicoAnalyzer.cxx
+++ PicoAnalyzer.cxx
@@ -497,7 +497,7 @@ void PicoAnalyzer(const Char_t *inFile = "/star/data01/pwg/dchen/Ana/fxtPicoAna/
     Double_t PsiTpcAllShifted=-999.0;
     // TPC Q-vector loop
     std::cout<<"size of vGoodTracks = "<<vGoodTracks.size()<<std::endl;
-    for(int i=0; i<vGoodTracks.size();i++){
+    for(unsigned int i=0; i<vGoodTracks.size();i++){
       StPicoTrack* picoTrack = vGoodTracks[i];
       Double_t pt = picoTrack->pPt();
       std::cout<<"pT = "<<pt<<std::endl;
