b6ead5dcad231c1e3870d8c1ccf52abfa7e522f4
cdxing <danielxchenx@gmail.com>
2020-05-25 01:22:02 -0400

    nMax to 3

--- PicoAnalyzer.cxx
+++ PicoAnalyzer.cxx
@@ -110,7 +110,7 @@ void PicoAnalyzer(const Char_t *inFile = "/star/data01/pwg/dchen/Ana/fxtPicoAna/
   EpdEpOutputName += ".root";
   StEpdGeom *mEpdGeom = new StEpdGeom();
   Double_t mThresh = 0.3; // EPD EP by hand
-  Double_t mMax = 2.0; // EPD EP by hand
+  Double_t mMax = 3.0; // EPD EP by hand
   Double_t etaRange[5] = {-5.16,-3.82,-3.28,-2.87,-2.60}; // EPD eta range to set 4 sub EPD EP
   TH2D wt("Order1etaWeight","Order1etaWeight",500,1.5,6.5,5,0,5);
   for (int ix=1; ix<501; ix++){
