88cfdee154a0469601a7b6b0a8dde7c38cbe771a
cdxing <danielxchenx@gmail.com>
2020-06-02 10:49:53 -0400

    hash map test

--- PicoAnalyzer.cxx
+++ PicoAnalyzer.cxx
@@ -24,7 +24,7 @@
 
 // C++ headers
 #include <iostream>
-#include <map>
+#include <unordered_map>
 #include <stdio.h>
 
 // ROOT headers
@@ -359,7 +359,7 @@ void PicoAnalyzer(const Char_t *inFile = "/star/data01/pwg/dchen/Ana/fxtPicoAna/
   }
   // ------------------ EPD & TPC event plane ab intio Correlations histograms ----------------------------------
   //Map test
-  std::map<int,int> mptest;
+  std::unordered_map<int,int> mptest;
   mptest.insert(pair<int, int>(1, 40));
   mptest.insert(pair<int, int>(2, 30));
   std::cout << "value 1: " << mptest.at(1);
