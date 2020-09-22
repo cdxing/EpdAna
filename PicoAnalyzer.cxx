358d5e29d3494610c8bd0a9907ba8e856646cefe
cdxing <danielxchenx@gmail.com>
2020-09-21 23:03:20 -0400

    absolute value

--- PicoAnalyzer.cxx
+++ PicoAnalyzer.cxx
@@ -371,6 +371,7 @@ void PicoAnalyzer(const Char_t *inFile = "/star/data01/pwg/dchen/Ana/fxtPicoAna/
   TProfile2D *mTpcShiftInput_sin[_nEventTypeBins_tpc], *mTpcShiftInput_cos[_nEventTypeBins_tpc]; // TPC EP input
   // TH1D* mPhiWeightInput[_nEventTypeBins];
   TString EpInputNameIni = "EpCorrection_INPUT_";
+  EpInputNameIni.Prepend("/star/u/dchen/GitHub/EpdAna/");
   EpInputNameIni.Append("sys_");
   EpInputNameIni.Append(sys_object[sys_cutN]);
   EpInputNameIni.Append(Form("_var%d_iter%d_", sys_varN, sys_iterN-1));
