7fb3f837223ef06c853a41c9a4c48417507974c1
cdxing <danielxchenx@gmail.com>
2021-01-05 11:25:43 -0500

    3 TPC event planes

--- PicoAnalyzer.cxx
+++ PicoAnalyzer.cxx
@@ -185,7 +185,7 @@ void PicoAnalyzer(const Char_t *inFile = "/star/data01/pwg/dchen/Ana/fxtPicoAna/
     }
   }
   // -------------------------- TPC event planes ----------------------------------
-  Double_t etaRange_tpc[2][5] = {{-2.0,-1.4,-1.0,-0.6,0.},{-2.0,-1.4,-1.0,-0.6,0.}}; // TPC eta range {-0.4, 0.0}
+  Double_t etaRange_tpc[2][5] = {{-2.0,-1.4,-1.3,-0.7,-0.6,0.},{-2.0,-1.4,-1.3,-0.7,-0.6,0.}}; // TPC eta range {-0.4, 0.0}
 
   TString ResoName = "Resolution_INPUT_sys_";
   ResoName.Prepend("/star/u/dchen/GitHub/EpdAna/");
@@ -288,7 +288,7 @@ void PicoAnalyzer(const Char_t *inFile = "/star/data01/pwg/dchen/Ana/fxtPicoAna/
     300,0,3.0,
     _nEventTypeBins_tpc,0,_nEventTypeBins_tpc);
     for (int ix=1; ix<301; ix++){
-      for (int iy=1; iy<3; iy++){
+      for (int iy=1; iy<7; iy++){
         double eta = wt_tpc[iOrder-1]->GetXaxis()->GetBinCenter(ix);
         if(iy==1) wt_tpc[iOrder-1]->SetBinContent(ix,iy,1);
         else {
