e007d7d520f65d4bb7c3eb34e29ccc13885f29fa
cdxing <danielxchenx@gmail.com>
2021-04-14 17:40:18 -0400

    dbg

--- PicoAnalyzer.sh
+++ PicoAnalyzer.sh
@@ -2,15 +2,15 @@
 #  This script is for test run the PicoAnalyzer.cxx
 
 #This is argument for iteration, if supplied
-iter=1
+iter="1"
 if [ $# -ge 1 ]
 then
-        iter=$1
+        iter="$1"
 fi
 stardev
 wait
 root4star -b -q -l RunAnalyzer.C+
 wait
-root4star -b -q -l PicoAnalyzer.cxx+'("st_physics_adc_19158057_raw_3500002.picoDst.root","test",1,0,0,3)'
+root4star -b -q -l PicoAnalyzer.cxx+'("st_physics_adc_19158057_raw_3500002.picoDst.root","test",1,0,0,$iter)'
 wait
 # chmod u+x
