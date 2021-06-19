b99dd9baa3efe41ad3884a01bb73a871e85c32ca
cdxing <danielxchenx@gmail.com>
2021-06-19 16:36:19 -0400

    test apply resolution

--- PicoAnalyzer.cxx
+++ PicoAnalyzer.cxx
@@ -238,7 +238,6 @@ void PicoAnalyzer(const Char_t *inFile = "/star/data01/pwg/dchen/Ana/fxtPicoAna/
     for(int i=0;i<_Ncentralities;i++){
       inputReso >> d_resolution[0][i];
       cout << "Resolution_11 "<<i <<": "<<d_resolution[0][i]<<endl;
-      d_resolution[1][i] = 1;
     }
   }
   inputReso.close();
